9947fc06bf7c37f9c7a3ebbe58dc0aebd5c4616b
matt <jmtooker@hotmail.com>
2021-10-24 23:27:13 -0500

    Basic React Todo Input box & Button

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,21 +1,38 @@
 import React, {useState} from 'react'
 
-function TodoList() {
-    const [input, setInput] = useState('')
+function TodoForm(props) {
+    const [input, setInput] = useState('');
+    //lets you type something in the box
+    const handleChange = e => {
+        setInput(e.target.value);
+    };
+    //keep todo list from refreshing
+    const handleSubmit = e => {
+        e.preventDefault();
+        //when submitting, you get a random id
+        // props.onSubmit({
+        //     id: Math.floor(Math.random() * 10000), 
+        //     text: input
+        // });
+
+        //makes the input in box vanish after hitting enter
+        setInput('');
+    }
     return (
         <div>
-            <form className="todo-form">
+            <form className="todo-form" onSubmit={handleSubmit}>
                 <input 
                     type="text" 
                     placeholder="Add a todo" 
                     value={input} 
                     name="text" 
                     className="todo-input"
+                    onChange={handleChange}
                 />
-                <button className="todo-button">Add</button>
+                <button className="todo-button">Add todo</button>
             </form>
         </div>
     )
 }
 
-export default TodoList
+export default TodoForm
